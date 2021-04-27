eaeab377fb43440780e8a3f3420c08e1619bb3c6
Melis A <am-back@pic.link>
2021-04-27 16:54:29 +0500

    second

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,35 @@ import a2 from './a2'
 import b1 from './b1'
 import c from './c'
 
-// HERE A1 task. To run it uncomment it 
+/////////////////////////////////////////////// HERE A1 task. To run it uncomment it 
+
+(()=>{
+    // you can try other input for testing function
+    let a1_input = '([abc])';
+    
+    let res = a1(a1_input)
+    console.log(res)
+})();
+
+
+/////////////////////////////////////////////// HERE A2 task. to Run it uncomment it
 
 // (()=>{
-//     let a1_input = '([abc])';
-//     let res = a1(a1_input)
-//     console.log(res)
-// })();
-
-
-// HERE A2 task. to Run it uncomment it
-
-// (()=>{
+//     // you can try other input for testing function
 //     let a2_input = 4;
+
 //     let res = a2(a2_input)
 //     console.log(res)
 // })();
 
 
 
-// HERE B1 task. to Run it uncomment it
+/////////////////////////////////////////////// HERE B1 task. to Run it uncomment it
 
 // (()=>{
+//     // you can try other input for testing function
 //     let b1_input = 5;
+
 //     let lru = new b1(b1_input)
 //     lru.set('a', 1)
 //     lru.set('b', 2)
@@ -37,11 +43,15 @@ import c from './c'
 //     console.log(lru.get('a'))
 // })();
 
-// HERE C task. to Run it uncomment it
+
+
+/////////////////////////////////////////////// HERE C task. to Run it uncomment it
 
 // (()=>{
+//     // you can try other input for testing function
 //     let c_input_n = ['abcd', 'cdef', 'ab', 'ef', 'ffff'];
 //     let c_input_k = ['abcdef', 'ffff'];
+
 //     c(c_input_n, c_input_k)
 
 // })();
