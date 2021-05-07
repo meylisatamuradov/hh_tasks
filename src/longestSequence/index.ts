cdb7804766a01f30531a78c4914c120be8d25389
Melis A <am-back@pic.link>
2021-05-07 17:28:34 +0500

    Optimization

--- src/longestSequence/index.ts
+++ src/longestSequence/index.ts
@@ -1,20 +1,18 @@
 type bit = '0' | '1';
 type zero= '0';
 export default (n:bit[]):zero[]=>{
-    var max:number =0;
-    let count:number=1;
-    for( let i=0; i<n.length-1; ++i ){
-        if(n[i] === '0'){
-            for(let j=i+1; j < n.length; ++j ){
-                if(n[j] !== '0') {
-                    if(count>max) max =count;
-                    i=j;
-                    count=1;
-                    break;
-                }
-                count++
+    let max:number =0;
+    let count:number=0;
+
+    for( let i=0; i<n.length; ++i ){ 
+        if(n[i] !== '0'){
+            if(count>max){
+                max=count;
             }
+            count = 0;
         }
+        if(n[i] === '0') count++;
     }
+    console.log(max)
     return Array.from( {length:max}, ()=>'0' )
 };
