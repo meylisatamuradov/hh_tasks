694e69b00b707261e14bd51e15e8ca6f93105068
Melis A <am-back@pic.link>
2021-05-07 16:58:36 +0500

    Optimization LongestSequence

--- src/longestSequence/index.ts
+++ src/longestSequence/index.ts
@@ -1,28 +1,20 @@
 type bit = '0' | '1';
 type zero= '0';
 export default (n:bit[]):zero[]=>{
-    var temp:number[] =[];
+    var max:number =0;
     let count:number=1;
     for( let i=0; i<n.length-1; ++i ){
         if(n[i] === '0'){
             for(let j=i+1; j < n.length; ++j ){
-                
                 if(n[j] !== '0') {
-                    if(count !== 1){
-                        temp.push(count)
-                        count=1;
-                    }
-                    
+                    if(count>max) max =count;
                     i=j;
+                    count=1;
                     break;
                 }
                 count++
             }
         }
     }
-    let result:zero[]=[];
-    for(let i=0; i< Math.max.apply(null, temp); ++i){
-        result.push('0')
-    }
-    return result
+    return Array.from( {length:max}, ()=>'0' )
 };
